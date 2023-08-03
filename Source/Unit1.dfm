0c60f788bedb28b56f3c06764919b8edc4e4b551
r57zone <r57zone@gmail.com>
2023-08-03 21:52:49 +0400

    Improvements: always on top of all windows, set & save position, and minimize on startup

--- Source/Unit1.dfm
+++ Source/Unit1.dfm
@@ -2,28 +2,26 @@ object Main: TMain
   Left = 0
   Top = 0
   Caption = 'Main'
-  ClientHeight = 469
-  ClientWidth = 800
+  ClientHeight = 468
+  ClientWidth = 796
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -11
   Font.Name = 'Tahoma'
   Font.Style = []
-  Position = poScreenCenter
+  Position = poDefault
   OnClose = FormClose
   OnCreate = FormCreate
   TextHeight = 13
   object EdgeBrowser: TEdgeBrowser
     Left = 0
     Top = 0
-    Width = 800
-    Height = 469
+    Width = 796
+    Height = 468
     Align = alClient
     TabOrder = 0
     UserDataFolder = '%LOCALAPPDATA%\bds.exe.WebView2'
     OnCreateWebViewCompleted = EdgeBrowserCreateWebViewCompleted
-    ExplicitWidth = 796
-    ExplicitHeight = 468
   end
 end
--- Source/Unit1.pas
+++ Source/Unit1.pas
@@ -31,8 +31,8 @@ type
 
 var
   Main: TMain;
-  WinOldWidth, WinOldHeight: integer;
-  WinSaveSize: boolean;
+  WinOldWidth, WinOldHeight, WinOldTop, WinOldLeft: integer;
+  WinSaveSize, WinSavePos: boolean;
   EdgeUserAgent: string;
 
 
@@ -44,14 +44,14 @@ procedure TMain.EdgeBrowserCreateWebViewCompleted(Sender: TCustomEdgeBrowser;
   AResult: HRESULT);
 var
   WebViewSettings: ICoreWebView2Settings2;
-  HR: HRESULT;
+  //HR: HRESULT;
 begin
   if Trim(EdgeUserAgent) = '' then Exit;
   // You must query SettingsInterface2 from SettingsInterface it's important
   Sender.SettingsInterface.QueryInterface(IID_ICoreWebView2Settings2, WebViewSettings);
   if not Assigned(WebViewSettings) then
     raise Exception.Create('ICoreWebView2Settings2 not found');
-  HR:=WebViewSettings.Set_UserAgent(PWideChar(EdgeUserAgent));
+  WebViewSettings.Set_UserAgent(PWideChar(EdgeUserAgent));
   //HR := WebViewSettings.Get_UserAgent(PWideChar(EdgeUserAgent));
   //if not SUCCEEDED(HR) then
     //raise Exception.Create('Get_UserAgent failed');
@@ -66,6 +66,14 @@ begin
       Ini:=TIniFile.Create(ExtractFilePath(ParamStr(0)) + 'Config.ini');
       Ini.WriteInteger('Window', 'Width', Width);
       Ini.WriteInteger('Window', 'Height', Height);
+      Ini.Free;
+    end;
+
+  if (WinSavePos) and (WindowState <> wsMaximized) then
+    if (WinOldTop <> Top) or (WinOldLeft <> Left) then begin
+      Ini:=TIniFile.Create(ExtractFilePath(ParamStr(0)) + 'Config.ini');
+      Ini.WriteInteger('Window', 'Top', Top);
+      Ini.WriteInteger('Window', 'Left', Left);
       Ini.Free;
     end;
 end;
@@ -90,33 +98,48 @@ begin
 
   EdgeBrowser.Navigate(URL);
 
+  // Windows sizes
   Width:=Ini.ReadInteger('Window', 'Width', Width);
   Height:=Ini.ReadInteger('Window', 'Height', Height);
   WinSaveSize:=Ini.ReadBool('Window', 'SaveSize', false);
+  WinOldWidth:=Width;
+  WinOldHeight:=Height;
 
+  // Windows position
+  if (Trim(Ini.ReadString('Window', 'Top', '')) <> '') and (Trim(Ini.ReadString('Window', 'Left', '')) <> '') then begin
+    Main.Top:=Ini.ReadInteger('Window', 'Top', Top);
+    Main.Left:=Ini.ReadInteger('Window', 'Left', Left);
+  end else begin
+    Main.Top:=Screen.Height div 2 - Height div 2; // Main.Position - Some problems with Edge
+    Main.Left:=Screen.Width div 2 - Width div 2;
+  end;
+  WinSavePos:=Ini.ReadBool('Window', 'SavePos', false);
+  WinOldTop:=Top;
+  WinOldLeft:=Left;
+
+  // Windows params
   if Ini.ReadBool('Window', 'HideMaximize', false) then
     BorderIcons:=Main.BorderIcons-[biMaximize];
-    
-  if Ini.ReadBool('Window', 'HideMinimize', false) then
-    BorderIcons:=Main.BorderIcons-[biMinimize];
-    
+
   case Ini.ReadInteger('Window', 'BorderStyle', 0) of
-    0: BorderStyle:=bsSizeable;
-    1: BorderStyle:=bsSingle;
-    2: BorderStyle:=bsDialog;
-    3: BorderStyle:=bsSizeToolWin;
-    4: BorderStyle:=bsToolWindow;
+    0: BorderStyle:=bsNone;
+    1: BorderStyle:=bsSizeable;
+    2: BorderStyle:=bsSingle;
+    3: BorderStyle:=bsDialog;
+    4: BorderStyle:=bsSizeToolWin;
+    5: BorderStyle:=bsToolWindow;
   end;
 
   case Ini.ReadInteger('Window', 'WindowState', 0) of
     1: WindowState:=wsMaximized;
     2: begin BorderStyle:=bsNone; WindowState:=wsMaximized; end;
-    //3: WindowState:=wsMinimized;
+    3: WindowState:=wsMinimized;
   end;
 
+  if Ini.ReadBool('Window', 'StayOnTop', false) then
+    FormStyle:=fsStayOnTop;
+
   Ini.Free;
-  WinOldWidth:=Width;
-  WinOldHeight:=Height;
 end;
 
 end.
